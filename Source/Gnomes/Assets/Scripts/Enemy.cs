26823cbd25351dabb2b33709b14b9011a326b181
ivovankreveld@hotmail.com <ivovankreveld@hotmail.com>
2015-11-12 17:32:33 +0100

    Merge branch 'ComponentProgrammingWebDatabase' of https://github.com/danielbrouwer08/EWI3620TU_Group_2
    Conflicts:
    	Core Document.docx

---
+++ Source/Gnomes/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public static Vector3 location1;
+    public static Vector3 location2;
+    public float speed = 5;
+    
+    private bool aware = false;
+    private Vector3 targetlocation = location1;
+    private Rigidbody rb;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void Update ()
+    {
+        if(!aware)
+        {
+            Patternwalk();
+        }    
+	}
+
+    void Patternwalk()
+    {
+        rb.AddRelativeForce(Vector3.forward*speed);
+        if((targetlocation - rb.position).magnitude < 1)
+        {
+            targetlocation = 
+        }
+    }
+
+}
