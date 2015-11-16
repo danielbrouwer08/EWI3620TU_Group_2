f90ff42b0f934b697ee17c3c3fd304e181a3faa7
sstroobants <stein.stroobants@live.nl>
2015-11-16 17:46:03 +0100

    no message

---
+++ Source/Gnomes/Assets/Scripts/CompanionScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompanionScript : MonoBehaviour {
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }        
+        }
+        Debug.Log(agent.isOnOffMeshLink);
+        if (agent.isOnOffMeshLink)
+        {
+            Debug.Log(agent.velocity.y);
+            if(agent.velocity.y < 0.0f)
+            {
+                agent.speed = 8;
+            }
+            else if(agent.velocity.y > 0.0f)
+            {
+                agent.speed = 0.5f;
+            }
+        }
+        else
+        {
+            agent.speed = 6;
+        }
+    }
+}
