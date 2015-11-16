bc06d65264114c109de1b2138176f4b5eae6f8de
ivovankreveld@hotmail.com <ivovankreveld@hotmail.com>
2015-11-16 17:40:35 +0100

    Skills "fly", "build" and "destroy" added

---
+++ Source/Gnomes/Assets/Scripts/Skills/Fly.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : MonoBehaviour {
+
+    private Rigidbody rb;
+    private PlayerScript ps;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        ps = GetComponent<PlayerScript>();
+    }
+	
+	void FixedUpdate ()
+    {
+        if (Input.GetButtonDown("Fire1") && Mathf.Abs(rb.velocity.y) >= 0.01)
+        {
+            rb.AddForce(new Vector3(0.0f, ps.jumpForce, 0.0f));
+        }
+    }
+}
