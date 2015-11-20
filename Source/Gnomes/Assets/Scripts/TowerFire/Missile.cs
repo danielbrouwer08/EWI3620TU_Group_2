b31b9b64316bc5d3083b37cee46f465c14fcc1bb
Stein Stroobants <stein.stroobants@live.nl>
2015-11-20 16:22:54 +0100

    Falling bridge and start of shooting tower thing

---
+++ Source/Gnomes/Assets/Scripts/TowerFire/Missile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+    private float speed = 10;
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        new GameObject()
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if (transform.position.Equals(target.position))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Source/Gnomes/Assets/Scripts/TowerFire/MissileScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileScript : MonoBehaviour {
+    public GameObject missile;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(missile, transform.position, transform.rotation);
+        }
+	}
+}
