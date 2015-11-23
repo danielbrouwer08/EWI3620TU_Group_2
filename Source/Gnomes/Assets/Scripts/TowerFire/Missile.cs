228fe389d7d4d38bba42fb5970e23f02d10f34ce
sstroobants <stein.stroobants@live.nl>
2015-11-23 15:03:55 +0100

    Falling bridge, shooting tower and updates to item pickups

--- Source/Gnomes/Assets/Scripts/TowerFire/Missile.cs
+++ Source/Gnomes/Assets/Scripts/TowerFire/Missile.cs
@@ -2,20 +2,19 @@
 using System.Collections;
 
 public class Missile : MonoBehaviour {
-    private float speed = 10;
-    public Transform target;
-
+    private GameObject target;
+    public float speed = 3f;
     // Use this for initialization
     void Start()
     {
-        new GameObject()
+        target = GameObject.FindGameObjectWithTag("TowerTarget");
     }
 	
 	// Update is called once per frame
 	void Update () {
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-        if (transform.position.Equals(target.position))
+        transform.position = Vector3.MoveTowards(transform.position, target.GetComponent<Transform>().position, step);
+        if(Vector3.Distance(transform.position, target.GetComponent<Transform>().position) < 0.0001)
         {
             Destroy(gameObject);
         }
--- Source/Gnomes/Assets/Scripts/TowerFire/MissileScript.cs
+++ Source/Gnomes/Assets/Scripts/TowerFire/MissileScript.cs
@@ -2,18 +2,21 @@
 using System.Collections;
 
 public class MissileScript : MonoBehaviour {
-    public GameObject missile;
+    public Rigidbody missile;
+    public float speed = 3f;
+
 
     // Use this for initialization
     void Start () {
-	    
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(missile, transform.position, transform.rotation);
+            Rigidbody clone = (Rigidbody) Instantiate(missile, transform.position, transform.rotation);
+            clone.velocity = transform.forward * speed;
         }
 	}
 }
