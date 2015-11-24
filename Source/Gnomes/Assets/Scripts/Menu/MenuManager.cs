f6b15110c7eb1f6e45b32ad4887f793706598431
MatthijsOttenhoff <mmottenhoff@gmail.com>
2015-11-24 11:09:51 +0100

    Basic animated main menu structure

---
+++ Source/Gnomes/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public MenuController currMenu;
+
+	// Use this for initialization
+	void Start () {
+        ShowMenu(currMenu);
+	}
+
+    public void ShowMenu(MenuController menu)
+    {
+        if (currMenu != null)
+            currMenu.IsOpen = false;
+
+        currMenu = menu;
+        currMenu.IsOpen = true;
+    }
+}
