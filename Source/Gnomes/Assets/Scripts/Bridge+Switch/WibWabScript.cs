cfbf0d0c0d32af593c6d53f84ca3d148b2ea27ee
sstroobants <stein.stroobants@live.nl>
2015-12-01 11:01:10 +0100

    Level 1 tijdelijk ok.

---
+++ Source/Gnomes/Assets/Scripts/Bridge+Switch/WibWabScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WibWabScript : MonoBehaviour {
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    
+	}
+
+    void OnCollisionStay(Collision other)
+    {
+        if(other.transform.position.z > 28.62f)
+        {
+            Debug.Log("groter");
+            rb.AddTorque(10000f, 0f, 0f);
+        }
+        else if(other.transform.position.z < 28.62f)
+        {
+            Debug.Log("kleiner");
+            rb.AddTorque(-10000f,0f, 0f);
+        }
+    }
+}
