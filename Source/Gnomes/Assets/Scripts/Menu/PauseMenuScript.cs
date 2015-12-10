baf9331ada8bb45cdf8d485ae2b25c1979281e5e
MatthijsOttenhoff <mmottenhoff@gmail.com>
2015-12-10 17:34:48 +0100

    Pause menu v0.1

---
+++ Source/Gnomes/Assets/Scripts/Menu/PauseMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseMenuScript : MonoBehaviour {
+
+    public MenuManager menu;
+    public MenuController pausemenu;
+    public Image menubackground;
+    public MenuController optionsmenu;
+
+    void Update () {
+        if (Input.GetKey("escape"))
+        {
+            PauseMenu();
+        }
+	}
+
+    void PauseMenu()
+    {
+        //Time.timeScale = 0; Still a problem atm, since also the menu's themselves will freeze because of this.
+        menu.ShowMenu(pausemenu);
+    }
+}
