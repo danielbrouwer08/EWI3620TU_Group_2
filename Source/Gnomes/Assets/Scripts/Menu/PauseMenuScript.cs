83a937c7c7ad8e11e41e5023e452f7836de4af95
MatthijsOttenhoff <mmottenhoff@gmail.com>
2015-12-11 16:14:02 +0100

    Pause menu interface v1.0 Done! Also fixed some minor animation bug in the main menu.

--- Source/Gnomes/Assets/Scripts/Menu/PauseMenuScript.cs
+++ Source/Gnomes/Assets/Scripts/Menu/PauseMenuScript.cs
@@ -2,23 +2,34 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class PauseMenuScript : MonoBehaviour {
+public class PauseMenuScript : MonoBehaviour
+{
 
     public MenuManager menu;
     public MenuController pausemenu;
-    public Image menubackground;
     public MenuController optionsmenu;
+    public BackgroundController backgroundimage;
+    public Button resumebutton;
 
-    void Update () {
+    void Update()
+    {
         if (Input.GetKey("escape"))
         {
             PauseMenu();
+            backgroundimage.IsPause = true;
         }
-	}
+        resumebutton.onClick.AddListener(delegate () { ResumeTime(); });
+    }
 
     void PauseMenu()
     {
-        //Time.timeScale = 0; Still a problem atm, since also the menu's themselves will freeze because of this.
+        Time.timeScale = 0; //Still a problem atm, since also the menu's themselves will freeze because of this.
         menu.ShowMenu(pausemenu);
+    }
+
+    void ResumeTime()
+    {
+        Time.timeScale = 1;
+        backgroundimage.IsPause = false;
     }
 }
