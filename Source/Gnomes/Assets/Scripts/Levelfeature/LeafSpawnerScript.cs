cced371e0ae2c9c9758d8c37356aa55aaf6cd21d
Stein Stroobants <stein.stroobants@live.nl>
2015-12-14 23:01:16 +0100

    Random blokjes laten spawnen in het water over een lijn (moet nog zorgen dat ze over een maximale range worden gedropt, en met een random interval)

---
+++ Source/Gnomes/Assets/Scripts/Levelfeature/LeafSpawnerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeafSpawnerScript : MonoBehaviour {
+    public Rigidbody leaf;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            float rnd = Random.value * 50;
+            Rigidbody clone = (Rigidbody)Instantiate(leaf, new Vector3(rnd, 0, 0), transform.rotation);
+        }
+    }
+}
