ca16c574bbc42b353bad66f2da4dec5bb6d92cea
MatthijsOttenhoff <mmottenhoff@gmail.com>
2015-12-15 11:05:31 +0100

    no message

--- Source/Gnomes/Assets/Scripts/Player/PlayerProperties.cs
+++ Source/Gnomes/Assets/Scripts/Player/PlayerProperties.cs
@@ -4,28 +4,25 @@ using System.Collections;
 
 public class PlayerProperties : MonoBehaviour {
 
-<<<<<<< HEAD
+
     public float startinghealth;
     public Slider healthbar;
     public float currenthealth;
-=======
+
     public float health;
 	public int score;
->>>>>>> 41e6782bdbc16c49496dc113108971a89452c9c1
+
 
     bool damaged;
     bool dead;
 
 	void Awake ()
     {
-<<<<<<< HEAD
         startinghealth = 100;
         currenthealth = startinghealth;
         healthbar.value = currenthealth;
-=======
 		score = 0;
         health = 100;
->>>>>>> 41e6782bdbc16c49496dc113108971a89452c9c1
 	}
 	
 	void Update ()
