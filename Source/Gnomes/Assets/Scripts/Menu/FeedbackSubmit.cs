9e76c0e44a4096e742d1b2ac2eab241db12c22e4
MatthijsOttenhoff <mmottenhoff@gmail.com>
2015-12-18 16:38:00 +0100

    V1.0 af

---
+++ Source/Gnomes/Assets/Scripts/Menu/FeedbackSubmit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class FeedbackSubmit : MonoBehaviour {
+
+    private Text feedbacktext;
+
+    public void Sendfeedback()
+    {
+        feedbacktext = GetComponent<Text>();
+        string fbtxt = feedbacktext.text;
+        Analytics.CustomEvent("feedback", new Dictionary<string, object>
+        {
+            { "feedback", fbtxt },
+
+        });
+        Debug.Log(fbtxt);
+    }
+    
+}
