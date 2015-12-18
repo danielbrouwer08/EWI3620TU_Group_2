2e9ba8e2ab20dcd3c1f7258c3e999f44f9755870
liekemichielsen <l.c.m.michielsen@student.tudelft.nl>
2015-12-18 17:11:10 +0100

    Level 7 werkt, muis kan verslagen worden

--- Source/Gnomes/Assets/Scripts/Enemies/EnemyProperties.cs
+++ Source/Gnomes/Assets/Scripts/Enemies/EnemyProperties.cs
@@ -4,12 +4,23 @@ using System.Collections;
 public class EnemyProperties : MonoBehaviour {
 
     public float health;
+    public GameObject[] wp;
+
+    void Start()
+    {
+        wp = GameObject.FindGameObjectsWithTag("WoodenPoleWall");
+    }
 	
 	void Update ()
     {
         if(health <= 0)
         {
+            Debug.Log(health);
             Destroy(gameObject);
+            for (int i=0; i<wp.Length; i++)
+            {
+                Destroy(wp[i]);
+            }
         }
 	}
 
