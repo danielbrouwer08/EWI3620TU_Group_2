2dfdfb78314fa2dd2e64fd2e8b8ae34848fc9022
liekemichielsen <l.c.m.michielsen@student.tudelft.nl>
2015-12-20 13:58:00 +0100

    Toverstokje gemaakt in blender, health wordt weer terug naar 100 gezet als je dood gaat, als je door het water loopt gaat je health naar beneden

---
+++ Source/Gnomes/Assets/WaterHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class WaterHealth : MonoBehaviour {
+
+    public int damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player in water");
+
+            other.GetComponent<PlayerProperties>().TakeDamage(damage);
+
+            Analytics.CustomEvent("playerInWater", new Dictionary<string, object>
+            {
+                { "x-location",  gameObject.transform.position.x},
+                { "z-location", gameObject.transform.position.z},
+                { "playerNum", other.gameObject.GetComponent<PlayerController>().playerNum }
+            });
+
+        }
+
+    }
+}
