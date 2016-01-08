91ed024085fa4ba5beafe7a636fd7c4923477b8f
MatthijsOttenhoff <mmottenhoff@gmail.com>
2016-01-08 16:49:37 +0100

    V0.1 geluid af

--- Source/Gnomes/Assets/Scripts/Menu/PauseMenuScript.cs
+++ Source/Gnomes/Assets/Scripts/Menu/PauseMenuScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.Analytics;
 using System.Collections.Generic;
 //using UnityEditor;
@@ -13,6 +14,9 @@ public class PauseMenuScript : MonoBehaviour
     public MenuController optionsmenu;
     public BackgroundController backgroundimage;
     public BackgroundController ingameinterface;
+    public AudioMixerSnapshot audiopaused;
+    public AudioMixerSnapshot audiounpaused;
+
     public Button resumebutton;
     public GameObject[] players;
 
@@ -57,7 +61,8 @@ public class PauseMenuScript : MonoBehaviour
         ingameinterface.IsPause = true;
         menu.ShowMenu(pausemenu);
         Paused = true;
-
+        audiopaused.TransitionTo(1f);
+        
     }
 
     void ResumeTime()
@@ -67,6 +72,7 @@ public class PauseMenuScript : MonoBehaviour
         ingameinterface.IsPause = false;
         pausemenu.IsOpen = false;
         Paused = false;
+        audiounpaused.TransitionTo(1f);
     }
 
     public void Mainmenu()
