d99ec0e69c16227fa1ec0a25e38cb63989faf373
Stein Stroobants <stein.stroobants@live.nl>
2016-01-11 13:33:26 +0100

    aanpassingen van de golf

---
+++ Source/Gnomes/Assets/Scripts/Levelfeature/WaveScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Time.deltaTime*(new Vector3(1,1,0)));
+	}
+}
