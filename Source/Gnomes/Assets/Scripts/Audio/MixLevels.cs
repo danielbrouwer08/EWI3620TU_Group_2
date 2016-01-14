a456f90cb6d961f81a565c2e3aa005e3aba95eaf
MatthijsOttenhoff <mmottenhoff@gmail.com>
2016-01-14 15:52:27 +0100

    Geluid Interface v0.1

---
+++ Source/Gnomes/Assets/Scripts/Audio/MixLevels.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class MixLevels : MonoBehaviour {
+
+    public AudioMixer MasterMixer;
+
+    public void SetMusiclvl(float musiclvl)
+    {
+        MasterMixer.SetFloat("MusicVolume", musiclvl);
+    }
+	
+    public void SetSFXlvl(float sfxlvl)
+    {
+        MasterMixer.SetFloat("SFXVolume", sfxlvl);
+    }
+}
