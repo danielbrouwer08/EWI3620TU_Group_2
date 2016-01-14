a2913cbb172ecbdf2168835c3165d0902ff3bdde
sstroobants <stein.stroobants@live.nl>
2016-01-14 16:48:26 +0100

    AI fixen en kleine bugfixen

--- Source/Gnomes/Assets/Scripts/Player/SwitchPlayerScript.cs
+++ Source/Gnomes/Assets/Scripts/Player/SwitchPlayerScript.cs
@@ -9,8 +9,15 @@ public class SwitchPlayerScript : MonoBehaviour {
         players = GameObject.FindGameObjectsWithTag("Player");
         if (PlayerPrefs.GetString("playermode") == "single")
         {
+            for(int i = 0; i < players.Length; i++) {
+                players[i].AddComponent<AIPath>();
+                players[i].GetComponent<AIPath>().target = players[1 - i].transform;
+                players[i].GetComponent<AIPath>().endReachedDistance = 2;
+                players[i].GetComponent<AIPath>().speed = 9;
+            }
             SinglePlayer = true;
             players[1].GetComponent<PlayerController>().enabled = false;
+            players[0].GetComponent<AIPath>().enabled = false;
         }
 	}
 	
@@ -24,7 +31,19 @@ public class SwitchPlayerScript : MonoBehaviour {
                 cur.GetComponent<PlayerController>().playerNum = 3 - cur.GetComponent<PlayerController>().playerNum;
                 cur.GetComponent<PlayerController>().enabled = !cur.GetComponent<PlayerController>().enabled;
                 cur.GetComponent<AIPath>().enabled = !cur.GetComponent<AIPath>().enabled;
+
+
             }
         }
-	}
+        if (Input.GetButtonDown("StopPlayer") && SinglePlayer)
+        {
+            foreach(GameObject cur in players)
+            {
+                if(!cur.GetComponent<PlayerController>().enabled)
+                {
+                    cur.GetComponent<AIPath>().canMove = !cur.GetComponent<AIPath>().canMove;
+                }
+            }
+        }
+    }
 }
