812722cec168ae26a0b1d59dd50dd4431db7ab56
sstroobants <stein.stroobants@live.nl>
2016-01-14 17:22:10 +0100

    beginnen aan aanpassen van de enemies (meerdere verschillende maken)

--- Source/Gnomes/Assets/Scripts/Dumbenemy/DumbEnemy.cs
+++ Source/Gnomes/Assets/Scripts/Dumbenemy/DumbEnemy.cs
@@ -5,7 +5,7 @@ public class DumbEnemy : MonoBehaviour {
 
     public float rotationspeed;
     private bool goback;
-
+    private int state;
     public GameObject pijl;
 
     private float nextFire;
@@ -14,20 +14,31 @@ public class DumbEnemy : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         transform.eulerAngles = new Vector3(0, 180, 0);
-        
+        state = 0;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.Rotate(0, rotationspeed, 0);
-
-        if (Time.time > nextFire)
+        switch (state)
         {
-            nextFire = Time.time + fireRate;
-            Vector3 rotation = transform.eulerAngles + new Vector3(0, 180, 0);
-            Instantiate(pijl, transform.position + new Vector3(0.2f, 3f, 0.2f), Quaternion.Euler(rotation));
-  
+            case 0:
+                transform.Rotate(0, rotationspeed, 0);
+
+                if (Time.time > nextFire)
+                {
+                    nextFire = Time.time + fireRate;
+                    Vector3 rotation = transform.eulerAngles + new Vector3(0, 180, 0);
+                    Instantiate(pijl, transform.position + new Vector3(0.2f, 3f, 0.2f), Quaternion.Euler(rotation));
+
+                }
+                break;
+            case 1:
+                //Vector3 new_rotation = Vector3.RotateTowards(transform.position, )
+                break;
+            default:
+                break;
         }
+
 
     }
 }
