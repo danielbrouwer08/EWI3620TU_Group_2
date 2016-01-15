e469452ddff79ec859daf65a2f6a5c346e036c97
MatthijsOttenhoff <mmottenhoff@gmail.com>
2016-01-15 12:04:12 +0100

    Geluid v0.4
    -  Checkpointgeluiden werkend.

--- Source/Gnomes/Assets/Scripts/Checkpoint/checkpoint.cs
+++ Source/Gnomes/Assets/Scripts/Checkpoint/checkpoint.cs
@@ -16,10 +16,11 @@ public class checkpoint : MonoBehaviour
     private GameObject gamemanager;
     private GameObject[] players;
     private CheckpointScript player;
-    public bool[] collided = new bool[2];
+    public bool[] collided;
 
     void Start()
     {
+        collided = new bool[2];
         gamemanager = GameObject.FindWithTag("GameManager");
         players = GameObject.FindGameObjectsWithTag("Player");
     }
@@ -100,14 +101,14 @@ public class checkpoint : MonoBehaviour
         if (collision.gameObject.tag.Equals("Player"))
         {
             player = collision.gameObject.GetComponent<CheckpointScript>();
-            //if (player.playernumber == 0)
-            //{
-            //    collided[0] = true;
-            //}
-            //if (player.playernumber == 1)
-            //{
-            //    collided[1] = true;
-            //}
+            if (player.playernumber == 0)
+            {
+                collided[0] = true;
+            }
+            if (player.playernumber == 1)
+            {
+                collided[1] = true;
+            }
 
             var dict2 = new Dictionary<string, object>();
             if (collision.gameObject.Equals(players[0]))
