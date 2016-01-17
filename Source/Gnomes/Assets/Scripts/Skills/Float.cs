7a64fdb205a3827c7eb06c090d9d3f912df786bb
liekemichielsen <l.c.m.michielsen@student.tudelft.nl>
2016-01-17 12:37:47 +0100

    Achievements menu is verwijderd en animatie voor het zweven is toegevoegd

--- Source/Gnomes/Assets/Scripts/Skills/Float.cs
+++ Source/Gnomes/Assets/Scripts/Skills/Float.cs
@@ -8,11 +8,13 @@ public class Float : MonoBehaviour
     public float floatvelocity = 0.8f;
     private bool floating = false;
     private int playerNum;
+    public Animation anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerNum = GetComponent<PlayerController>().playerNum;
+        anim = GetComponent<Animation>();
     }
 
     void FixedUpdate()
@@ -31,6 +33,11 @@ public class Float : MonoBehaviour
         if (floating && rb.velocity.y < -floatvelocity)
         {
             rb.velocity = new Vector3(rb.velocity.x, -floatvelocity, rb.velocity.z);
+        }
+        if (floating)
+        {
+            Debug.Log("Speel animatie zweven");
+            anim.Play("Zweven");
         }
     }
 }
