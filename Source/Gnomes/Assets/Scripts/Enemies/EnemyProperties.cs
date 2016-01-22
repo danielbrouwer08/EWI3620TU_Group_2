e500acd525ff21b052ee92cdca76aa5b9c4edd52
danielbrouwer08 <danielbrouwer08@hotmail.com>
2016-01-22 13:31:13 +0100

    Muis visuele feedback

--- Source/Gnomes/Assets/Scripts/Enemies/EnemyProperties.cs
+++ Source/Gnomes/Assets/Scripts/Enemies/EnemyProperties.cs
@@ -7,7 +7,7 @@ public class EnemyProperties : MonoBehaviour {
     public GameObject[] wp;
 	private GameObject body;
 	private Shader standardShader;
-	public float visualHitTime = 0.4f;
+	public float visualHitTime = 0.1f;
 	public Color[] colors = new Color[2];
 
     void Start()
@@ -40,8 +40,8 @@ public class EnemyProperties : MonoBehaviour {
 	}
 
 	IEnumerator visualHit(){
-		body.GetComponent<Renderer>().material.color = colors[0];
-		yield return new WaitForSeconds(visualHitTime);
 		body.GetComponent<Renderer>().material.color = colors[1];
+		yield return new WaitForSeconds(visualHitTime);
+		body.GetComponent<Renderer>().material.color = colors[0];
 	} 
 }
